54b8b8d1faafa458f5aecc725e60dadd92be876f
Miron Sorin <smiron86@gmail.com>
2013-07-14 15:35:16 +0300

    Work in progress @ porting to WritableBitmap in WPF

---
+++ NET/v3/cnnnet/cnnnet.ViewerWpf/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using cnnnet.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cnnnet.ViewerWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Fields
+
+        private const int NetworkWidth = 800;
+        private const int NetworkHeight = 600;
+
+        private Thread _networkProcessThread;
+        private CnnNet _network;
+        private ViewerDesirability _viewerDesirability;
+
+        #endregion
+
+        #region Methods
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            _network = new CnnNet(NetworkWidth, NetworkHeight);
+            _viewerDesirability = new ViewerDesirability(_network);
+
+            _viewerDesirability.Start();
+            img.Source = _viewerDesirability.WriteableBitmap;
+            _networkProcessThread.Start();
+        }
+
+        private void NetworkProcessThreadStart()
+        {
+            while (true)
+            {
+                _network.Process();
+            }
+        }
+
+        #endregion
+
+        #region Instance
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _networkProcessThread = new Thread(NetworkProcessThreadStart);
+            _networkProcessThread.IsBackground = true;
+        }
+
+        #endregion
+    }
+}
---
+++ NET/v3/cnnnet/cnnnet.ViewerWpf/ViewerDesirability.cs
@@ -0,0 +1,49 @@
+﻿using cnnnet.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace cnnnet.ViewerWpf
+{
+    public class ViewerDesirability : Viewer
+    {
+        #region Fields
+        
+        private CnnNet _network;
+
+        #endregion
+
+        #region Methods
+
+        protected override Color[,] GetData()
+        {
+            var desirabilityMap = (double[,])_network.NeuronDesirabilityMap.Clone();
+            var result = new Color[_network.Height, _network.Width];
+
+            for (int y = 0; y < _network.Height; y++)
+            {
+                for (int x = 0; x < _network.Width; x++)
+                {
+                    result[y, x] = Color.FromRgb((byte)0, (byte)(desirabilityMap[y, x] * 255), (byte)0);
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region Instance
+
+        public ViewerDesirability(CnnNet network)
+            : base(network.Width, network.Height)
+        {
+            _network = network;
+        }
+
+        #endregion
+    }
+}
