6f64e63a96a52f493a8f6e0e09e3f68ac8b37a9d
Miron Sorin <smiron86@gmail.com>
2013-07-14 23:12:58 +0300

    Massive Performance

--- NET/v3/cnnnet/cnnnet.ViewerWpf/ViewerManager.cs
+++ NET/v3/cnnnet/cnnnet.ViewerWpf/ViewerManager.cs
@@ -23,7 +23,12 @@ namespace cnnnet.ViewerWpf
 
         private readonly int _stride;
         private readonly int _bytesPerPixel;
-        private Int32Rect _sourceRect;
+
+        private Int32Rect _writableBitmapSourceRect;
+        private Rect _neuronIconDestRect;
+        private Rect _neuronIconSourceRect;
+
+        private Thread _preRenderThread;
 
         #endregion
 
@@ -41,6 +46,11 @@ namespace cnnnet.ViewerWpf
 
         #region Methods
 
+        public void RegisterViewer(ViewerBase viewer)
+        {
+            _viewers.Add(viewer);
+        }
+
         public void Update(double elapsed)
         {
             using (_writableBitmap.GetBitmapContext())
@@ -55,45 +65,59 @@ namespace cnnnet.ViewerWpf
         {
             foreach (var neuron in _network.Neurons)
             {
-                _writableBitmap.Blit(new Rect(neuron.PosX, neuron.PosY, Resources.NeuronIdle.PixelWidth, Resources.NeuronIdle.PixelHeight), Resources.NeuronIdle,
-                    new Rect(0, 0, Resources.NeuronIdle.PixelWidth, Resources.NeuronIdle.PixelHeight));
+                _neuronIconDestRect.X = neuron.PosX;
+                _neuronIconDestRect.Y = neuron.PosY;
+
+                _writableBitmap.Blit(_neuronIconDestRect, Resources.NeuronIdle, _neuronIconSourceRect);
             }
         }
 
         private void UpdateBackground()
         {
-            var viewersWithData = _viewers.Select(viewer => new
-            {
-                Viewer = viewer,
-                Data = viewer.GetData()
-            }).ToArray();
-
-            for (int y = 0; y < _network.Height; y++)
-            {
-                for (int x = 0; x < _network.Width; x++)
-                {
-                    int bitmapDataIndex = (y * _network.Width + x) * _bytesPerPixel;
-
-                    _bitmapData[bitmapDataIndex + Constants.ColorRedIndex] = 0;
-                    _bitmapData[bitmapDataIndex + Constants.ColorGreenIndex] = 0;
-                    _bitmapData[bitmapDataIndex + Constants.ColorBlueIndex] = 0;
-
-                    foreach (var viewerWithData in viewersWithData)
-                    {
-                        _bitmapData[bitmapDataIndex + Constants.ColorRedIndex] += viewerWithData.Data[y, x * viewerWithData.Viewer.BytesPerPixel + Constants.ColorRedIndex];
-                        _bitmapData[bitmapDataIndex + Constants.ColorGreenIndex] += viewerWithData.Data[y, x * viewerWithData.Viewer.BytesPerPixel + Constants.ColorGreenIndex];
-                        _bitmapData[bitmapDataIndex + Constants.ColorBlueIndex] += viewerWithData.Data[y, x * viewerWithData.Viewer.BytesPerPixel + Constants.ColorBlueIndex];
-                    }
-                }
-            };
-
-            _writableBitmap.WritePixels(_sourceRect, _bitmapData, _stride, 0);
-            
+            _writableBitmap.WritePixels(_writableBitmapSourceRect, _bitmapData, _stride, 0);
         }
 
-        public void RegisterViewer(ViewerBase viewer)
+        private void PreRender()
         {
-            _viewers.Add(viewer);
+            try
+            {
+                while (true)
+                {
+                    byte[] tmpBitmapData = new byte[_network.Height * _network.Width * _bytesPerPixel];
+
+                    var viewersWithData = _viewers.Select(viewer => new
+                    {
+                        Viewer = viewer,
+                        Data = viewer.GetData()
+                    }).ToArray();
+
+                    for (int y = 0; y < _network.Height; y++)
+                    {
+                        for (int x = 0; x < _network.Width; x++)
+                        {
+                            int bitmapDataIndex = (y * _network.Width + x) * _bytesPerPixel;
+
+                            tmpBitmapData[bitmapDataIndex + Constants.ColorRedIndex] = 0;
+                            tmpBitmapData[bitmapDataIndex + Constants.ColorGreenIndex] = 0;
+                            tmpBitmapData[bitmapDataIndex + Constants.ColorBlueIndex] = 0;
+
+                            foreach (var viewerWithData in viewersWithData)
+                            {
+                                tmpBitmapData[bitmapDataIndex + Constants.ColorRedIndex] += viewerWithData.Data[y, x * viewerWithData.Viewer.BytesPerPixel + Constants.ColorRedIndex];
+                                tmpBitmapData[bitmapDataIndex + Constants.ColorGreenIndex] += viewerWithData.Data[y, x * viewerWithData.Viewer.BytesPerPixel + Constants.ColorGreenIndex];
+                                tmpBitmapData[bitmapDataIndex + Constants.ColorBlueIndex] += viewerWithData.Data[y, x * viewerWithData.Viewer.BytesPerPixel + Constants.ColorBlueIndex];
+                            }
+                        }
+                    };
+
+                    _bitmapData = tmpBitmapData;
+
+                    Thread.Sleep(30);
+                }
+            }
+            catch
+            {
+            }
         }
 
         #endregion
@@ -110,7 +134,14 @@ namespace cnnnet.ViewerWpf
             _stride = _writableBitmap.PixelWidth * _bytesPerPixel;
 
             _bitmapData = new byte[_network.Height * _network.Width * _bytesPerPixel];
-            _sourceRect = new Int32Rect(0, 0, _writableBitmap.PixelWidth, _writableBitmap.PixelHeight);
+
+            _writableBitmapSourceRect = new Int32Rect(0, 0, _writableBitmap.PixelWidth, _writableBitmap.PixelHeight);
+            _neuronIconDestRect = new Rect(0, 0, Resources.NeuronIdle.PixelWidth, Resources.NeuronIdle.PixelHeight);
+            _neuronIconSourceRect = new Rect(0, 0, Resources.NeuronIdle.PixelWidth, Resources.NeuronIdle.PixelHeight);
+
+            _preRenderThread = new Thread(PreRender);
+            _preRenderThread.IsBackground = true;
+            _preRenderThread.Start();
         }
 
         #endregion
